35deb9cc6336bb5c4ab0e382b057822bc70382f1
Tristan Weber <echo1990@gmx.de>
2016-05-11 18:15:15 +0200

    puk points are detected and editable with mouse

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,83 +1,184 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
+	ofSetFullscreen(true);
     ofEnableSmoothing();
-    
+	ofSetCircleResolution(32);
+
+	setupGui();
     tuioClient.start(3333);
-    
+
+	tuioChanged = false;
+
+	ofAddListener(gui->newGUIEvent, this, &ofApp::guiEvent);
+
     ofAddListener(tuioClient.cursorAdded,this,&ofApp::tuioAdded);
 	ofAddListener(tuioClient.cursorRemoved,this,&ofApp::tuioRemoved);
 	ofAddListener(tuioClient.cursorUpdated,this,&ofApp::tuioUpdated);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update()
+{
     tuioClient.getMessage();
+
+	if (tuioChanged && tuioContacts.size() == contactCount->getValue())
+	{
+		pukContacts.clear();
+		for (auto tuio : tuioContacts)
+		{
+			pukContacts.push_back(pukContact());
+			pukContacts.back().contact.sid = tuio.sid;
+			pukContacts.back().contact.position = tuio.position;
+		}
+	}
+
+	tuioChanged = false;
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
 	ofClear(0, 0);
     tuioClient.drawCursors();
-}
 
-void ofApp::tuioAdded(ofxTuioCursor &tuioCursor){
-	ofPoint loc = ofPoint(tuioCursor.getX()*ofGetWidth(),tuioCursor.getY()*ofGetHeight());
-	cout << "Point n" << tuioCursor.getSessionId() << " add at " << loc << endl;
-}
-
-void ofApp::tuioUpdated(ofxTuioCursor &tuioCursor){
-	ofPoint loc = ofPoint(tuioCursor.getX()*ofGetWidth(),tuioCursor.getY()*ofGetHeight());
-	cout << "Point n" << tuioCursor.getSessionId() << " updated at " << loc << endl;
-}
-
-void ofApp::tuioRemoved(ofxTuioCursor &tuioCursor){
-	ofPoint loc = ofPoint(tuioCursor.getX()*ofGetWidth(),tuioCursor.getY()*ofGetHeight());
-	cout << "Point n" << tuioCursor.getSessionId() << " remove at " << loc << endl;
+	for (auto contact : pukContacts)
+	{
+		contact.draw(contact.intersects(ofPoint(ofGetMouseX(), ofGetMouseY())));
+	}
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::exit()
+{
+	delete gui;
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::setupGui()
+{
+	gui = new ofxUISuperCanvas("TUIO Puk Editor", 0, 0, 200, 110);
 
+	gui->addSpacer();
+	pukFileName = gui->addTextInput("Puk file name: ", "puk.xml");
+	pukFileName->setAutoUnfocus(false);
+
+	contactCount = gui->addIntSlider("Contact points count: ", 2, 10, 3);
+
+	gui->addLabelButton("Write file", false);
+
+	gui->setTheme(ofxUIThemeType::OFX_UI_THEME_BLUEBLUE);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::guiEvent(ofxUIEventArgs &e)
+{
+	string name = e.widget->getName();
+	int kind = e.widget->getKind();
 
+	if (kind == OFX_UI_WIDGET_LABELBUTTON)
+	{
+		ofxUIButton *button = (ofxUIButton *)e.widget;
+		if (button->getValue() > 0) {
+			cout << name << ": " << pukFileName->getTextString() << endl;
+		}
+	}
+}
+
+void ofApp::tuioAdded(ofxTuioCursor &tuioCursor)
+{
+	tuioContacts.push_back(tuioContact());
+	tuioContacts.back().sid = tuioCursor.getSessionId();
+	tuioContacts.back().position.set(tuioCursor.getX() * ofGetWidth(), tuioCursor.getY() * ofGetHeight());
+
+	tuioChanged = true;
+}
+
+void ofApp::tuioUpdated(ofxTuioCursor &tuioCursor)
+{
+	int cursorIndex = getTuioPointIndex(tuioCursor.getSessionId());
+	if (cursorIndex < 0)
+	{
+		return;
+	}
+	tuioContacts.at(cursorIndex).position.set(tuioCursor.getX() * ofGetWidth(), tuioCursor.getY() * ofGetHeight());
+
+	tuioChanged = true;
+}
+
+void ofApp::tuioRemoved(ofxTuioCursor &tuioCursor)
+{
+	int cursorIndex = getTuioPointIndex(tuioCursor.getSessionId());
+	if (cursorIndex >= 0)
+	{
+		tuioContacts.erase(tuioContacts.begin() + cursorIndex);
+	}
+
+	tuioChanged = true;
+}
+
+int ofApp::getTuioPointIndex(int sid)
+{
+	int cursorIndex = -1;
+	for (int i = 0; i < tuioContacts.size(); ++i)
+	{
+		if (tuioContacts.at(i).sid == sid)
+		{
+			cursorIndex = i;
+			break;
+		}
+	}
+	return cursorIndex;
+}
+
+int ofApp::getPukContactIndex(int sid)
+{
+	int cursorIndex = -1;
+	for (int i = 0; i < pukContacts.size(); ++i)
+	{
+		if (pukContacts.at(i).contact.sid == sid)
+		{
+			cursorIndex = i;
+			break;
+		}
+	}
+	return cursorIndex;
+}
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button)
+{
+	ofPoint currentMousePosition(x, y);
+	for (auto& contact : pukContacts)
+	{
+		if (contact.clicked)
+		{
+			contact.contact.position.set(contact.contact.position + (currentMousePosition - lastMousePosition));
+		}
+	}
+
+	lastMousePosition.set(x, y);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button)
+{
+	for (auto& contact : pukContacts)
+	{
+		if (contact.intersects(ofPoint(x, y)))
+		{
+			contact.clicked = true;
+		}
+	}
+	lastMousePosition.set(x, y);
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::mouseReleased(int x, int y, int button)
+{
+	for (auto& contact : pukContacts)
+	{
+		contact.clicked = false;
+	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,27 +1,79 @@
 #pragma once
 
+#define OFX_UI_NO_XML
+
 #include "ofMain.h"
+#include "ofxUi.h"
 #include "ofxTuio.h"
 
 class ofApp : public ofBaseApp{
-public:
+private:
+	struct tuioContact
+	{
+	public:
+		long sid;
+		ofPoint position;
+	};
+
+	struct pukContact
+	{
+	public:
+		tuioContact contact;
+		bool clicked = false;
+
+		void draw(bool highlight)
+		{
+			ofPushStyle();
+			if (highlight)
+			{
+				ofSetColor(HIGHLIGHT_COLOR);
+			}
+			else
+			{
+				ofSetColor(OUTER_COLOR);
+			}
+			ofCircle(contact.position, RADIUS);
+			ofSetColor(INNER_COLOR);
+			ofCircle(contact.position, INNER_RADIUS);
+			ofPopStyle();
+		}
+
+		bool intersects(ofPoint location)
+		{
+			return contact.position.distanceSquared(location) <= pow(RADIUS, 2);
+		}
+
+	private:
+		const ofColor OUTER_COLOR = ofColor(255, 255, 255, 64);
+		const ofColor HIGHLIGHT_COLOR = ofColor(0, 150, 210, 128);
+		const ofColor INNER_COLOR = ofColor(0, 154, 213, 255);
+		const float RADIUS = 50;
+		const float INNER_RADIUS = 5;
+	};
+
     void setup();
     void update();
     void draw();
+	void exit();
 
-    void keyPressed  (int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
+	void setupGui();
+	void guiEvent(ofxUIEventArgs &e);
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
     void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+    void tuioAdded(ofxTuioCursor & tuioCursor);
+	void tuioRemoved(ofxTuioCursor & tuioCursor);
+	void tuioUpdated(ofxTuioCursor & tuioCursor);
 
-    void	tuioAdded(ofxTuioCursor & tuioCursor);
-	void	tuioRemoved(ofxTuioCursor & tuioCursor);
-	void	tuioUpdated(ofxTuioCursor & tuioCursor);
-    
-    ofxTuioClient   tuioClient;
+	int getTuioPointIndex(int sid);
+	int getPukContactIndex(int sid);
+
+	ofxUISuperCanvas *gui;
+	ofxUITextInput *pukFileName;
+	ofxUIIntSlider *contactCount;
+    ofxTuioClient tuioClient;
+	ofPoint lastMousePosition;
+	vector<tuioContact> tuioContacts;
+	vector<pukContact> pukContacts;
+	bool tuioChanged;
 };
